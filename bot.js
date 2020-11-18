205e4d8321f1af43cbb8ce9de29a9398f218d365
Zachary Bennett <zack@Zacharys-MacBook-Pro.local>
2020-11-18 18:35:14 -0500

    finished graph, ready for future features.

--- bot.js
+++ bot.js
@@ -27,18 +27,19 @@ let fetchCrypto = async(message) => {
   let currentPrice = fetchCoinInfo.data.market_data.current_price.usd;
   if (currentPrice >= 1) currentPrice = currentPrice.toFixed(2);  
   // assets for embed card
-  let image = fetchCoinInfo.data.image.large
+  let tokenLogo = fetchCoinInfo.data.image.large; 
   let homepage = fetchCoinInfo.data.links.homepage[0];
   let change24 = fetchCoinInfo.data.market_data.price_change_percentage_24h.toFixed(2);
   let change7d = fetchCoinInfo.data.market_data.price_change_percentage_7d.toFixed(2);
   let change1yr = fetchCoinInfo.data.market_data.price_change_percentage_1y.toFixed(2)
+  let change1Month = fetchCoinInfo.data.market_data.price_change_percentage_30d.toFixed(2); 
   if (change24 >= 0.1) change24 = "+" + change24; 
   if (change7d >= 0.1) change7d = "+" + change7d; 
   if (change1yr >= 0.1) change1yr = "+" + change1yr; 
   // gets coin's price data for chart 
   let chartData = []
   chartData = await CoinGeckoClient.coins.fetchMarketChart(tokenId, {
-    days: "365"
+    days: "30"
   });
   chartData = chartData.data.prices; 
   let filterChartData = chartData.map(value => {
@@ -51,36 +52,44 @@ let fetchCrypto = async(message) => {
     let dateForChart = []; 
     let priceForChart = []; 
     for (let i = 0; i < filterChartData.length; i++) {
-      filterChartData[i].day = moment().format("MMM Do 20")
+      filterChartData[i].day = moment(filterChartData[i].day).format("MMM Do 20")
       if (filterChartData[i].price >= 1) filterChartData[i].price = filterChartData[i].price.toFixed(2)
       dateForChart.push(filterChartData[i].day)
       priceForChart.push(parseFloat(filterChartData[i].price))
     }
     // console.log(priceForChart)
-    console.log(dateForChart)
+    // console.log(dateForChart)
+    // console.log(image)
     return {
       tokenName, 
       currentPrice,
-      image,
+      tokenLogo,
       homepage,
       tokenId,
       tokenSymbol,
       change24,
       change7d,
       change1yr, 
+      change1Month,
       dateForChart,
       priceForChart
     }
 }
 
 let makeEmbedCard = async(message, coinData) => {
+    let bgColor;
+    if (coinData.change1Month > 0) {
+        bgColor = 'rgba(0, 255, 0, 0.8)';
+    } else {
+      bgColor = 'rgba(255, 0, 0, 0.8)';
+    }
   const embed = new Discord.MessageEmbed()
     .setColor('#43ff0a')
     .setTitle(coinData.tokenName + " " + "(" + coinData.tokenSymbol.toUpperCase() + ")")
     .setURL(coinData.homepage)
     .setAuthor('Crypticker Bot', 'https://cdn2.iconfinder.com/data/icons/cryptocurrency-cryptocurrencies/128/cryptocurrency_cryptocurrencies_4_bitcoin-512.png', 'https://github.com/fnyah/Crypticker-discord')
     .setDescription('https://www.coingecko.com/en/coins/' + coinData.tokenId)
-    .setThumbnail(coinData.image)
+    .setThumbnail(coinData.tokenLogo)
     .addFields(
       { name: 'Current Price:', value: "$" + coinData.currentPrice + " USD"},
       { name: '24 Hour Change:', value: coinData.change24 + "%", inline: true },
@@ -88,7 +97,7 @@ let makeEmbedCard = async(message, coinData) => {
       { name: 'One Year Change:', value: coinData.change1yr + "%", inline: true },
     )
     .setTimestamp()
-    const width = 600;
+    const width = 700;
     const height = 400;
     var type = 'bar';
     const canvasRenderService = new CanvasRenderService(width, height, (ChartJS) => { });
@@ -97,19 +106,35 @@ let makeEmbedCard = async(message, coinData) => {
       data: {
         labels: coinData.dateForChart,
         datasets: [{
-            label: 'Change over one year',
+            label: 'Change over one month ($USD)',
             data: coinData.priceForChart,
-            backgroundColor: 'rgba(0, 255, 10, 0.5)',
+            backgroundColor: bgColor,
         }]
     },
     options: {
-      scales: {
-          yAxes: [{
-              ticks: {
+      legend: {
+        display: false, 
+        labels: {
+          fontColor: "white",
+          fontSize: 25
+        },
+      },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fontColor: "white",
+                    fontSize: 20,
+                    beginAtZero: true
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                  fontColor: "white",
+                  fontSize: 20,
                   beginAtZero: false
-              }
-          }]
-      }
+                } 
+            }]
+        }
   }
     });
     const attachment = new Discord.MessageAttachment(image, "image.png");
@@ -124,7 +149,7 @@ client.on('message', async (message) => {
   let coinData; 
   if (!message.author.equals(client.user) && message.content.startsWith(PREFIX)) {
     coinData = await fetchCrypto(message).catch((err) => {
-      console.log(message.reply("Please enter a valid coin name or symbol.", err))
+      message.reply("Please enter a valid coin name or symbol.", err)
    })
     makeEmbedCard(message, coinData);
   }
